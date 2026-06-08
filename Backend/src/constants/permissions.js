a1b7f9d43e4edc1365659e38c13468f5cc589164
Omar Yasser <omar156.mh@gmail.com>
2026-06-08 08:19:03 +0300

    Add Permissons

--- Backend/src/constants/permissions.js
+++ Backend/src/constants/permissions.js
@@ -1,8 +1,29 @@
+// create constents for permissions
 const PERMISSIONS = {
-  USERS: "users",
-  ROLES: "roles",
-  RECIPES: "recipes",
-  INGREDIENTS: "ingredients",
+    USERS_VIEW: "users.view",
+    USERS_CREATE: "users.create",
+    USERS_EDIT: "users.edit",
+    USERS_DELETE: "users.delete",
+
+    ROLES_VIEW: "roles.view",
+    ROLES_CREATE: "roles.create",
+    ROLES_EDIT: "roles.edit",
+    ROLES_DELETE: "roles.delete",
+
+    RECIPE_VIEW: "recipes.view",
+    RECIPE_CREATE: "recipes.create",
+    RECIPE_EDIT: "recipes.edit",
+    RECIPE_DELETE: "recipes.delete",
+
+    INGREDIENTS_VIEW: "ingredients.view",
+    INGREDIENTS_CREATE: "ingredients.create",
+    INGREDIENTS_EDIT: "ingredients.edit",
+    INGREDIENTS_DELETE: "ingredients.delete",
+
+    CATEGORY_VIEW: "categories.view",
+    CATEGORY_CREATE: "categories.create",
+    CATEGORY_EDIT: "categories.edit",
+    CATEGORY_DELETE: "categories.delete",
 };
 
-module.exports = PERMISSIONS;
+module.exports = permissions;
